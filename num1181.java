58f7e85a793fcc47cd768170f756fcd98b71ae0a
eugene <81062608+eugene225@users.noreply.github.com>
2022-05-19 01:03:58 +0900

    Create num1181.java
    Comparator 개념 공부 및 암기 필요

---
+++ num1181.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Comparator;
+
+public class Main {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			
+			int N;
+			N = sc.nextInt();
+			
+			String st[] = new String[N];
+            
+            sc.nextLine();
+			
+			for(int i=0;i<N;i++) {
+				String s = sc.next();
+				st[i] = s;
+			}
+			
+			//comparator 개념 공부
+			Arrays.sort(st, new Comparator<String>() {
+				public int compare(String st1, String st2) {
+					//길이가 같으면, 둘 중 사전식 배열 빠른것부터 정렬
+					if(st1.length()==st2.length()) {
+						return st1.compareTo(st2);
+					}else { //길이가 같지 않으면, 길이가 작은 단어부터 정렬
+						return st1.length()-st2.length();
+					}
+				}
+			});
+			
+			System.out.println(st[0]);
+			
+			for(int i=1;i<N;i++) {
+				if(!st[i].equals(st[i-1])) {
+					System.out.println(st[i]);
+				}
+			}
+			
+			sc.close();
+		}
+}
