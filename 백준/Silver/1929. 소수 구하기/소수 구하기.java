651e57d5cc0b3b5a99fbdf65fe696b43c0b2a844
eugene <81062608+eugene225@users.noreply.github.com>
2022-07-11 14:17:53 +0900

    [Silver III] Title: 소수 구하기, Time: 852 ms, Memory: 29556 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static boolean[] prime;
+
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        prime = new boolean[N+1];
+        get_prime();
+
+        for(int i=M;i<=N;i++){
+            if(!prime[i]) System.out.println(i);
+        }
+    }
+
+    public static void get_prime() {
+        // true = 소수아님 , false = 소수
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
+
+}
